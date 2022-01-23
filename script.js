fdd7560e8e1e9cc4a0b5990c3d0765be66bd9d8d
frontendpodyplomowe <frontendpodyplomowe@gmail.com>
2022-01-23 12:46:41 +0100

    Update gitignore for script

---
+++ script.js
@@ -0,0 +1,18 @@
+const password = document.querySelector("#field2");
+password.addEventListener('keyup', ()=>{
+    password.value = password.value.trim();
+
+    const regex = /^(?=.*[^\w])(?=.*\d).{8,}$/g;
+    if(password.value){
+        if(password.value.match(regex)){
+            password.classList.remove("password-invalid");
+            password.classList.add("password-valid");
+
+        } else{
+            password.classList.remove("password-valid");
+            password.classList.add("password-invalid");
+        }
+    } else{
+        password.classList.remove("password-valid", "password-invalid");
+    }
+})
